091360b48fb2c8d86a2fd96fc4a73e7e17c91185
Samuel Gélineau <sgelineau@innobec.com>
2015-04-08 12:27:01 -0400

    Detect the four arrow keys

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up"))
+			print ("up key was pressed");
+		if (Input.GetKeyDown ("down"))
+			print ("down key was pressed");
+		if (Input.GetKeyDown ("left"))
+			print ("left key was pressed");
+		if (Input.GetKeyDown ("right"))
+			print ("right key was pressed");
+	}
+}
